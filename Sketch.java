7146c3eef5a404a128d7c549fb2a0312dcb22f69
me <me>
2022-05-16 19:59:43 -0700

    joy

--- Sketch.java
+++ Sketch.java
@@ -2,57 +2,60 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
-  public void settings() {
-    size(400, 400);
-  }
-
-  public void setup() {
-    background(0);
-  }
-
-  public void draw() {
-    background(0);
-    fill(255);
-    ellipse(mouseX, mouseY, 10, 10);
-
-	Point a = new Point(0, 0);
-	Point b = new Point(20, 30);
-	Point c = new Point(60, 0);
-	for ( int i = 0; i < 100; i++) {
-		i /= 100;
-		Point p = bezier(new Point[]{a, b, c}, i);
-		ellipse((float) p.x, (float) p.y, 10F, 10F);
-
+	public void settings() {
+		size(400, 400);
 	}
 
+	public void setup() { 
+		background(0);
+	}
 
-  }
+	public void draw() {
+		background(255);
+		fill(255);
+		ellipse(mouseX, mouseY, 10, 10);
 
-	Point bezier(Point[] points, double percent) {
+		Point a = new Point(0, 0);
+		Point b = new Point(20, 30);
+		Point c = new Point(60, 0);
+
+
+		for ( int i = 0; i < 100; i++) {
+			float p = i / 100F;
+			Point bp = bezier(new Point[]{a, b, c}, p);
+			point(bp.x, bp.y);
+
+		}
+	}
+
+	Point bezier(Point[] points, float percent) {
 		if ( points.length == 0) return null;
 		if ( points.length == 1 ) return points[0];
 		Point[] rpoints = new Point[points.length-1];
 		for ( int i = 0; i<rpoints.length; i++) {
 			rpoints[i] = lerp(points[i], points[i+1], percent);
 		}
-		bezier(rpoints, percent);
-		return null;
+		return bezier(rpoints, percent);
 	}
 
-	Point lerp(Point a, Point b, double percent) {
-		double cx = a.x + (b.x-a.x) * percent;
-		double cy = a.y + (b.y -a.y) * percent;	
+	Point lerp(Point a, Point b, float percent) {
+		float cx = a.x + (b.x-a.x) * percent;
+		float cy = a.y + (b.y -a.y) * percent;	
 		return new Point(cx, cy);
 	}
 }
 
 class Point {
-	public double x;
-	public double y;
+	public float x;
+	public float y;
 	
-	public Point(double x, double y) {
+	public Point(float x, float y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	public void print() {
+		System.out.println("x: " + x + " y: " + y);
 	}
 	
 }
