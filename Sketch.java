a4e575555297bf0c0877347bbbb23dfe0a021963
me <me>
2022-05-16 20:23:05 -0700

    its so coooool

--- Sketch.java
+++ Sketch.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+	ArrayList<Point> points = new ArrayList<>();
 
 	public void settings() {
 		size(400, 400);
@@ -8,32 +11,65 @@ public class Sketch extends PApplet {
 
 	public void setup() { 
 		background(0);
+		// points.add( new Point(0, 50) );
+		// points.add( new Point(30, 20) );
+		// points.add( new Point(90, 300) );
+		// points.add( new Point(200, 30) );
 	}
 
+	@Override
+	public void mouseClicked() {
+		Point clickPos = new Point(mouseX, mouseY);
+		points.add(clickPos);
+	}
+	
 	public void draw() {
 		background(255);
 		fill(255);
 		ellipse(mouseX, mouseY, 10, 10);
 
-		Point a = new Point(0, 0);
-		Point b = new Point(20, 30);
-		Point c = new Point(60, 0);
 
+		if ( points.size() != 0) {
+			for ( int i = 0; i < 100; i++) {
+				float p = i / 100F;
+				Point bp = bezier(points, p);
+				point(bp.x, bp.y);
+			}
 
-		for ( int i = 0; i < 100; i++) {
-			float p = i / 100F;
-			Point bp = bezier(new Point[]{a, b, c}, p);
-			point(bp.x, bp.y);
-
+			for ( Point p : points) {
+				ellipse(p.x, p.y, 10, 10);
+			}
 		}
 	}
 
+	/**
+	 * use for make silly smooth curve on a list of points
+	 * @param points
+	 * @param percent
+	 * @return
+	 */
 	Point bezier(Point[] points, float percent) {
 		if ( points.length == 0) return null;
 		if ( points.length == 1 ) return points[0];
 		Point[] rpoints = new Point[points.length-1];
 		for ( int i = 0; i<rpoints.length; i++) {
 			rpoints[i] = lerp(points[i], points[i+1], percent);
+		}
+		return bezier(rpoints, percent);
+	}
+
+	/**
+	 * use for make silly smooth curve on a list of points
+	 * @param points
+	 * @param percent
+	 * @return
+	 */
+	Point bezier(ArrayList<Point> points, float percent) {
+		if ( points.size() == 0) return null;
+		if ( points.size() == 1 ) return points.get(0);
+		Point[] rpoints = new Point[points.size()-1];
+		for ( int i = 0; i<rpoints.length; i++) {
+			rpoints[i] = lerp(points.get(i), points.get(i+1), percent);
 		}
 		return bezier(rpoints, percent);
 	}
@@ -57,5 +93,4 @@ class Point {
 	public void print() {
 		System.out.println("x: " + x + " y: " + y);
 	}
-	
 }
